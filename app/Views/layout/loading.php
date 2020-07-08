e4f21992cbb5639965aa91dfc275abfea79b2a5c
Arif Jundi <momo_jundi99@ymail.com>
2020-07-08 14:31:36 +0700

    layout settings

---
+++ app/Views/layout/loading.php
@@ -0,0 +1,14 @@
+<div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="assets/images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+<!-- Page Loader-->
+<div id="page-loader">
+    <div class="page-loader-body"><img src="assets/images/logo-default-95x80.png" alt="" width="95" height="80" />
+        <div class="cssload-wrapper">
+            <div class="cssload-border">
+                <div class="cssload-whitespace">
+                    <div class="cssload-line"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="page">
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,121 @@
+<!-- Page Header-->
+<header class="section page-header">
+    <!-- RD Navbar-->
+    <div class="rd-navbar-wrap">
+        <nav class="rd-navbar rd-navbar-classic" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-sm-device-layout="rd-navbar-fixed" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-xl-device-layout="rd-navbar-static" data-xl-layout="rd-navbar-static" data-stick-up-clone="false" data-md-stick-up-offset="74px" data-lg-stick-up-offset="66px" data-md-stick-up="true" data-lg-stick-up="true">
+            <div class="rd-navbar-outer">
+                <div class="rd-navbar-inner">
+                    <!-- RD Navbar Panel-->
+                    <div class="rd-navbar-panel">
+                        <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                        <!-- RD Navbar Brand-->
+                        <div class="rd-navbar-brand"><a class="brand" href="index.html">
+                                <div class="brand__name"><img class="brand__logo-dark" src="assets/images/logo-default-95x80.png" alt="" width="150" height="80" /><img class="brand__logo-mobile" src="assets/images/logo-mobile-170x50.png" alt="" width="170" height="50" />
+                                </div>
+                            </a></div>
+                    </div>
+                    <div class="rd-navbar-body">
+                        <!-- RD Navbar Aside-->
+                        <div class="rd-navbar-aside">
+                            <div class="rd-navbar-content-outer">
+                                <div class="rd-navbar-content__toggle rd-navbar-static--hidden" data-rd-navbar-toggle=".rd-navbar-content"><span></span></div>
+                                <div class="rd-navbar-content">
+                                    <ul class="list-bordered list-inline">
+                                        <li>
+                                            <dl class="list-terms-inline">
+                                                <dt>24/7 Support</dt>
+                                                <dd><a href="tel:#">1-800-700-6200</a></dd>
+                                            </dl>
+                                        </li>
+                                        <li>
+                                            <dl class="list-terms-inline">
+                                                <dt>E-mail</dt>
+                                                <dd><a href="mailto:#">mail@demolink.org</a></dd>
+                                            </dl>
+                                        </li>
+                                        <li>
+                                            <ul class="list-inline list-inline-xs">
+                                                <li><a class="icon icon-gray-dark icon-style-brand fa fa-facebook" href="#"></a></li>
+                                                <li><a class="icon icon-gray-dark icon-style-brand fa fa-twitter" href="#"></a></li>
+                                                <li><a class="icon icon-gray-dark icon-style-brand fa fa-google-plus" href="#"></a></li>
+                                                <li><a class="icon icon-gray-dark icon-style-brand fa fa-pinterest-p" href="#"></a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="rd-navbar-panel__button"><a class="button button-xs button-icon button-icon-left button-default button-ujarak" href="login.html"><span class="icon mdi mdi-account"></span>Login</a></div>
+                        </div>
+                        <!-- RD Navbar Nav Wrap-->
+                        <div class="rd-navbar-nav-wrap">
+                            <div class="rd-navbar-element">
+                                <!-- RD Navbar Search-->
+                                <div class="rd-navbar-search rd-navbar-search-toggled">
+                                    <button class="rd-navbar-search-toggle" data-rd-navbar-toggle=".rd-navbar-search"></button>
+                                    <form class="rd-search" action="search-results.html" data-search-live="rd-search-results-live" method="GET">
+                                        <div class="form-wrap">
+                                            <input class="form-input" id="rd-navbar-search-form-input" type="text" name="s" autocomplete="off">
+                                        </div>
+                                        <button class="rd-navbar-search-submit" type="submit"></button>
+                                        <label class="form-label" for="rd-navbar-search-form-input">Search...</label>
+                                        <div class="rd-search-results-live" id="rd-search-results-live"></div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- RD Navbar Nav-->
+                            <ul class="rd-navbar-nav">
+                                <li><a href="about.html">Home</a>
+                                </li>
+                                <li><a href="services.html">Our Product</a>
+                                    <ul class="rd-navbar-dropdown">
+                                        <li><a href="blog-post.html">IT Solution</a></li>
+                                        <li><a href="blog-post.html">Cyber Security</a></li>
+                                        <li><a href="blog-post.html">Web Development</a></li>
+                                        <li><a href="blog-post.html">E-Commerce</a></li>
+                                        <li><a href="blog-post.html">Energy</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="pricing.html">Our Services</a>
+                                    <ul class="rd-navbar-megamenu">
+                                        <li>
+                                            <ul class="rd-megamenu-list">
+                                                <li><a href="faq.html">Finance and Banking System Development</a></li>
+                                                <li><a href="login.html">eCommerce Solutions</a></li>
+                                                <li><a href="cart-page.html">Web and Mobile Development</a></li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            <ul class="rd-megamenu-list">
+                                                <li><a href="404-page.html">Cross Platform Solution</a></li>
+                                                <li><a href="503-page.html">Project Management</a></li>
+                                                <li><a href="coming-soon.html">Cybersecurity</a></li>
+                                                <li><a href="maintenance.html">Research and Development</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                                <li>
+                                    <a href="blog.html">Client & Partners</a>
+                                </li>
+                                <li>
+                                    <a href="promos.html">About Us</a>
+                                    <ul class="rd-navbar-dropdown">
+                                        <li><a href="blog-post.html">Profile Company</a></li>
+                                        <li><a href="blog-post.html">Visi, Misi, Corporate Value</a></li>
+                                        <li><a href="blog-post.html">Work Culture & Ethic</a></li>
+                                        <li><a href="blog-post.html">Award & Certifications Product</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact-us.html">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
