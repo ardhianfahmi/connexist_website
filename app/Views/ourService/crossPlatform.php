acf53183dc15d3459fd89184c80a9cfdf7020afa
ardhianfahmi <you@example.com>
2020-07-22 13:53:35 +0700

    push

--- app/Views/ourService/crossPlatform.php
+++ app/Views/ourService/crossPlatform.php
@@ -16,15 +16,15 @@
   <section class="breadcrumbs-custom">
     <div class="breadcrumbs-custom__aside bg-image context-dark" style="background-image: url(images/bg-image-1-1920x900.jpg);">
       <div class="container">
-        <h2 class="breadcrumbs-custom__title">Energy</h2>
+        <h2 class="breadcrumbs-custom__title">Our Service</h2>
       </div>
     </div>
     <div class="breadcrumbs-custom__main bg-gray-light">
       <div class="container">
         <ul class="breadcrumbs-custom__path">
           <li><a href="index.html">Home</a></li>
-          <li><a href="#">Our Products</a></li>
-          <li class="active">Energy</li>
+          <li><a href="#">Our Service</a></li>
+          <li class="active">Finance and Banking System Development</li>
         </ul>
       </div>
     </div>
@@ -34,14 +34,153 @@
     <div class="container">
       <div class="row row-50 justify-content-md-center">
         <div class="col-md-9 col-lg-8 col-xl-6">
-          <h3>Grid system</h3>
-          <p class="big">A responsive fluid grid system that appropriately scales up to 12 columns with gorgeous full-width and boxed options.</p>
+          <h3>Finance and Banking System Development</h3>
+          <p class="big">Kami mengandalkan keahlian teknologi dan pengalaman industri khusus untuk mengembangkan segala jenis aplikasi web, seluler, desktop, dan hybrid untuk keperluan perbankan dan bisnis keuangan. Dari konsep hingga kode, pengembangan hingga penerapan, kami menyediakan solusi IT khusus yang mempercepat alur kerja, mengoptimalkan operasi, dan meningkatkan pendapatan.</p>
         </div>
       </div>
     </div>
   </section>
 
-
+  <!-- Why Choose Us-->
+  <section class="section section-sm bg-white text-center">
+    <div class="container">
+      <h2>Layanan Kami</h2>
+      <div class="row row-30 wow fadeIn">
+        <div class="col-md-6 col-lg-4">
+          <!-- Box Alice-->
+          <article class="box-alice">
+            <div class="box-alice__inner">
+              <div class="box-alice__aside">
+                <div class="box-alice__icon-outer">
+                  <div class="icon-figure">
+                    <div class="box-triangle">
+                      <svg x="0px" y="0px" width="80px" height="80px" viewBox="0 0 100 100" style="fill: #f7f7f7;">
+                        <path d="M20,93.301c-11,0-15.5-7.794-10-17.321l30-51.962c5.5-9.526,14.5-9.526,20,0l30,51.962 c5.5,9.526,1,17.321-10,17.321H20z"></path>
+                      </svg>
+                    </div><span class="box-alice__icon linearicons-cog"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="box-alice__main">
+                <h5 class="box-alice__title">High Quality Hardware</h5>
+                <p>We use top-notch hardware to develop the most efficient apps for our customers.</p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <!-- Box Alice-->
+          <article class="box-alice">
+            <div class="box-alice__inner">
+              <div class="box-alice__aside">
+                <div class="box-alice__icon-outer">
+                  <div class="icon-figure">
+                    <div class="box-triangle">
+                      <svg x="0px" y="0px" width="80px" height="80px" viewBox="0 0 100 100" style="fill: #f7f7f7;">
+                        <path d="M20,93.301c-11,0-15.5-7.794-10-17.321l30-51.962c5.5-9.526,14.5-9.526,20,0l30,51.962 c5.5,9.526,1,17.321-10,17.321H20z"></path>
+                      </svg>
+                    </div><span class="box-alice__icon linearicons-headset"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="box-alice__main">
+                <h5 class="box-alice__title">Dedicated 24\7 Support</h5>
+                <p>You can rely on our 24/7 tech support that will gladly solve any app issue you may have.</p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <!-- Box Alice-->
+          <article class="box-alice">
+            <div class="box-alice__inner">
+              <div class="box-alice__aside">
+                <div class="box-alice__icon-outer">
+                  <div class="icon-figure">
+                    <div class="box-triangle">
+                      <svg x="0px" y="0px" width="80px" height="80px" viewBox="0 0 100 100" style="fill: #f7f7f7;">
+                        <path d="M20,93.301c-11,0-15.5-7.794-10-17.321l30-51.962c5.5-9.526,14.5-9.526,20,0l30,51.962 c5.5,9.526,1,17.321-10,17.321H20z"></path>
+                      </svg>
+                    </div><span class="box-alice__icon linearicons-wallet"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="box-alice__main">
+                <h5 class="box-alice__title">30-Day Money-back Guarantee</h5>
+                <p>If you are not satisfied with our apps, we will return your money in the first 30 days.</p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <!-- Box Alice-->
+          <article class="box-alice">
+            <div class="box-alice__inner">
+              <div class="box-alice__aside">
+                <div class="box-alice__icon-outer">
+                  <div class="icon-figure">
+                    <div class="box-triangle">
+                      <svg x="0px" y="0px" width="80px" height="80px" viewBox="0 0 100 100" style="fill: #f7f7f7;">
+                        <path d="M20,93.301c-11,0-15.5-7.794-10-17.321l30-51.962c5.5-9.526,14.5-9.526,20,0l30,51.962 c5.5,9.526,1,17.321-10,17.321H20z"></path>
+                      </svg>
+                    </div><span class="box-alice__icon linearicons-rocket"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="box-alice__main">
+                <h5 class="box-alice__title">Agile and Fast Working Style</h5>
+                <p>This type of approach to our work helps our specialists to quickly develop better apps.</p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <!-- Box Alice-->
+          <article class="box-alice">
+            <div class="box-alice__inner">
+              <div class="box-alice__aside">
+                <div class="box-alice__icon-outer">
+                  <div class="icon-figure">
+                    <div class="box-triangle">
+                      <svg x="0px" y="0px" width="80px" height="80px" viewBox="0 0 100 100" style="fill: #f7f7f7;">
+                        <path d="M20,93.301c-11,0-15.5-7.794-10-17.321l30-51.962c5.5-9.526,14.5-9.526,20,0l30,51.962 c5.5,9.526,1,17.321-10,17.321H20z"></path>
+                      </svg>
+                    </div><span class="box-alice__icon linearicons-phone"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="box-alice__main">
+                <h5 class="box-alice__title">Some Apps are Free</h5>
+                <p>We also develop free apps that can be downloaded online without any payments.</p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="col-md-6 col-lg-4">
+          <!-- Box Alice-->
+          <article class="box-alice">
+            <div class="box-alice__inner">
+              <div class="box-alice__aside">
+                <div class="box-alice__icon-outer">
+                  <div class="icon-figure">
+                    <div class="box-triangle">
+                      <svg x="0px" y="0px" width="80px" height="80px" viewBox="0 0 100 100" style="fill: #f7f7f7;">
+                        <path d="M20,93.301c-11,0-15.5-7.794-10-17.321l30-51.962c5.5-9.526,14.5-9.526,20,0l30,51.962 c5.5,9.526,1,17.321-10,17.321H20z"></path>
+                      </svg>
+                    </div><span class="box-alice__icon linearicons-thumbs-up"></span>
+                  </div>
+                </div>
+              </div>
+              <div class="box-alice__main">
+                <h5 class="box-alice__title">High Level of Usability</h5>
+                <p>All our products have high usability allowing users to easily operate the apps.</p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <?= $this->include("layout/footer.php") ?>
 </body>
